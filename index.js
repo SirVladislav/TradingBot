a1ccff4a4272d8a4cffc113b227d5a84c3302cda
sirvladislav <sir.vladislav.programming@gmail.com>
2021-04-15 21:49:54 +0300

    asda

---
+++ index.js
@@ -0,0 +1,27 @@
+const Binance = require('node-binance-api');
+const binance = new Binance().options({
+  APIKEY: '<key>',
+  APISECRET: '<secret>'
+});
+
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.listen(3000)
+
+app.post('/', function (req, res) {
+  res.json(req.body);
+console.log(req.body['text']);
+    if(req.body['text'] == 'Buy'){
+
+    }else if(req.body['text'] == 'Sell'){
+
+    }
+
+})
+
+
+
+async function getInfo(){
+    console.info( await binance.futuresTime() );
+}
